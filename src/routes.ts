5906cc701ae92b28e87141b0dfb7a7639423c227
Henrique Almeida - Frontall USA / BON <halmeida.bon@frontallusa.com>
2024-01-19 11:55:58 -0300

    fix:fixing function name

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,6 @@ const router: Router = Router();
 
 router.get('/balance', (req, res) => getBalance(req, res));
 
-router.post('/reset', (req: Request, res: Response) => getBalance(req, res));
+router.post('/reset', (req: Request, res: Response) => postReset(req, res));
 
 export default router;
