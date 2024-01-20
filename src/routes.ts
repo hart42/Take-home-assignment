3ffd7a9e4fa6661e0cfd4fb3b4ce2524e7d54c23
Henrique Almeida - Frontall USA / BON <halmeida.bon@frontallusa.com>
2024-01-19 22:12:38 -0300

    feat: creating the /event route with deposit option

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,23 @@
 import { Router, Request, Response } from 'express';
-import { getBalance, postReset } from './controllers/events';
+import { getBalance, postReset, postDeposit } from './controllers/events';
 
 const router: Router = Router();
 
 router.get('/balance', (req, res) => getBalance(req, res));
 
 router.post('/reset', (req: Request, res: Response) => postReset(req, res));
+
+router.post('/event', (req: Request, res: Response) => {
+    const { type } = req.body;
+    switch (type) {
+        case 'deposit':
+            postDeposit(req, res);
+            break;
+    
+        default:
+            res.status(404).json(0)
+            break;
+    }
+})
 
 export default router;
